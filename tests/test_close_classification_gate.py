244fa1b40e54e7b9b5def6df398ba8690d7f815c
Fernando Angelin <fernando_angelin@hotmail.com>
2024-07-13 11:58:02 +0100

    fix: spacing in test case classification gate

--- tests/test_close_classification_gate.py
+++ tests/test_close_classification_gate.py
@@ -9,22 +9,23 @@ from src.setup.setup_browser import browser
 from time import sleep
 
 def test_close_classification_gate_mobile_view(browser):
-
     wait = WebDriverWait(browser, 5)
     url = "https://www.twitch.tv/leva2k"
-
     browser.get(url)
-    sleep(5)
+    # wait to make sure the page is loaded
+    sleep(2)
+
     reject_cookies_button = wait.until(
         EC.presence_of_element_located((By.CSS_SELECTOR, Homepage.reject_cookies_button_selector))
     )
     reject_cookies_button.click()
-    
+
+    # This is a workaround to close the cookies modal
     delete_cookies_modal(browser)
+
+    # wait to make sure the page is loaded
     sleep (2)
     info("Accepting classification gate if displayed")
     accept_classification_gate(browser)
-
-    sleep(5)
-
-    browser.save_screenshot("screenshot.png")
+    # wait to make sure the page is loaded
+    sleep(2)
