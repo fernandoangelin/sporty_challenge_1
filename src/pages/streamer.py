dbc8baf5e91c2c999ea3036f986d2e61d6bb424a
Fernando Angelin <fernando_angelin@hotmail.com>
2024-08-25 13:24:04 +0100

    chore: add first video selector to the page objects

--- src/pages/streamer.py
+++ src/pages/streamer.py
@@ -1,2 +1,3 @@
 class Streamer:
+    first_video_selector = 'article > div > div > img'
     classification_gate_button_selector = '[data-a-target="content-classification-gate-overlay-start-watching-button"]'
